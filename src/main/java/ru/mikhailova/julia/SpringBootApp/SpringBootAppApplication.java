3835d3c18155a9f0f0eaf88adbcd213e83ea6a50
Mikhaylova-Sdobnova <yuliana.semenyuk-mihailova@yandex.ru>
2025-03-10 15:20:46 +0300

    a small change

--- src/main/java/ru/mikhailova/julia/SpringBootApp/SpringBootAppApplication.java
+++ src/main/java/ru/mikhailova/julia/SpringBootApp/SpringBootAppApplication.java
@@ -1,7 +1,8 @@
-package ru.mikhailova.julia.SpringBootApp.Configuration;
+package ru.mikhailova.julia.SpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
